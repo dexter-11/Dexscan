4f56332cc3b8f6955883ec7b54797e25bd2d62b9
Mekhla Upadhyay <65162001+meks-0202@users.noreply.github.com>
2022-10-14 04:51:45 +0530

    Update dexscan.sh

--- dexscan.sh
+++ dexscan.sh
@@ -223,3 +223,20 @@ fi
 ELAPSED="$(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
 printf "${YELLOW}[+] Duration: ${NC}$ELAPSED\n"
 printf "${BLUE}[+] DONE.${NC}\n"
+
+
+##################################################################################################################################################################
+
+if [ "$1" == "" ]
+    echo "You forgot IP address! Sample execution --> ./ipsweep.h 192.168.2"   
+else
+    for ip in `seq 1 254` ; do
+        ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &  
+        # ['&' allows threading else it would go one IP at a time]
+    done
+fi
+
+#               OR
+# ping -c 1 192.168.1.$ip     # just hardcode it in the bash script
+
+##################################################################################################################################################################
